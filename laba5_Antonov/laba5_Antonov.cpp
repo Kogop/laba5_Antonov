0d3f3cc1bb2fa5171ca75297e42ce466af2552f6
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-09-09 00:55:52 +0300

    getting ready

--- laba5_Antonov/laba5_Antonov.cpp
+++ laba5_Antonov/laba5_Antonov.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 #include <mpi.h>
 #include <fstream>
 //#include <math.h>
@@ -7,21 +8,21 @@
 using namespace std;
 
 const int root = 0, tag = 0;
-const int n = 100, m = 150;
+const int n = 4, m = 4;
 double A[n][m], B[m][n], v[n], d[n], C[n][n], A1[n][m], B1[m][n], v1[n];
 
 void FillMatrix(double(&AA)[n][m], double(&BB)[m][n]) {
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++) {
-			AA[i][j] = rand() % 100 / 2.4;
+			AA[i][j] = 1/*random() % 100 / 2.4*/;
 		}
 	}
 
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++) {
-			BB[i][j] = rand() % 100 / 2.4;
+			BB[i][j] = 1/*random() % 100 / 2.4*/;
 		}
 	}
 }
@@ -52,8 +53,9 @@ void Matrix_Peremnoj_na_vector(double(&AA)[n][m], double(&vv)[n]) {
 	}
 }
 
+
 void Zapis_v_File() {
-	ofstream File1("C:\\Users\\neste\\source\\Repos\\SuperCompModel\\laba2\\laba2\\Matrix_1.txt");
+	ofstream File1("/home/vc/18VF1/laba2-master/laba2_try3/Matrix_1.txt");
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++) {
@@ -63,7 +65,7 @@ void Zapis_v_File() {
 	}
 	File1.close();
 
-	ofstream File2("C:\\Users\\neste\\source\\Repos\\SuperCompModel\\laba2\\laba2\\Matrix_2.txt");
+	ofstream File2("/home/vc/18VF1/laba2-master/laba2_try3/Matrix_2.txt");
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++) {
@@ -73,7 +75,7 @@ void Zapis_v_File() {
 	}
 	File2.close();
 
-	ofstream File4("C:\\Users\\neste\\source\\Repos\\SuperCompModel\\laba2\\laba2\\Vector_1.txt");
+	ofstream File4("/home/vc/18VF1/laba2-master/laba2_try3/Vector_1.txt");
 	for (int i = 0; i < n; i++)
 	{
 		File4 << v[i] << endl;
@@ -81,12 +83,12 @@ void Zapis_v_File() {
 	File4.close();
 }
 
-void Zapix_otvetov_v_File(double(&CC)[n][n]) {
-	ofstream File3("C:\\Users\\neste\\source\\Repos\\SuperCompModel\\laba2\\laba2\\Matrix_Otvet1.txt");
+void Zapix_otvetov_v_File(double(&CC)[n][n]/*,double(&d)[n]*/) {
+	ofstream File3("/home/vc/18VF1/laba2-master/laba2_try3/Matrix_Otvet1.txt");
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++) {
-			File3 << CC[i][j] << "\t";
+			File3 << CC[i][j] << " ";
 		}
 		File3 << "\n";
 	}
@@ -100,16 +102,16 @@ void Zapix_otvetov_v_File(double(&CC)[n][n]) {
 		cout << endl;
 	}
 
-	//ofstream File5("C:\\Users\\neste\\source\\Repos\\SuperCompModel\\laba2\\laba2\\Vector_Otvet1.txt");
-	//for (int i = 0; i < n; i++)
-	//{
-	//	File5 << d[i] << endl;
-	//}
-	//File5.close();
+	/*ofstream File5("/home/vc/18VF1/laba2-master/laba2_try3/Vector_Otvet1.txt");
+	for (int i = 0; i < n; i++)
+	{
+		File5 << d[i] << endl;
+	}
+	File5.close();*/
 }
 
 void read_Vector() {
-	ifstream File5("C:\\Users\\neste\\source\\Repos\\SuperCompModel\\laba2\\laba2\\Vector_1.txt");
+	ifstream File5("/home/vc/18VF1/laba2-master/laba2_try3/Vector_1.txt");
 	for (int i = 0; i < n; i++) {
 		File5 >> v1[i];
 		//  cout << DD[i]<<endl;
@@ -118,7 +120,7 @@ void read_Vector() {
 }
 
 void read_Matrix() {
-	ifstream File1("C:\\Users\\neste\\source\\Repos\\SuperCompModel\\laba2\\laba2\\Matrix_1.txt");
+	ifstream File1("/home/vc/18VF1/laba2-master/laba2_try3/Matrix_1.txt");
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++) {
@@ -129,7 +131,7 @@ void read_Matrix() {
 	}
 	File1.close();
 
-	ifstream File2("C:\\Users\\neste\\source\\Repos\\SuperCompModel\\laba2\\laba2\\Matrix_2.txt");
+	ifstream File2("/home/vc/18VF1/laba2-master/laba2_try3/Matrix_2.txt");
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++) {
@@ -144,20 +146,22 @@ void read_Matrix() {
 double k[m];
 double l[m];
 
-void vzat_vector_iz_matrix(double(&AA)[n][m], int s1, double(&BB)[m][n], int s2/*, int c*/) {
+void vzat_vector_iz_matrix(/*double(&AA)[n][m], int s1,*/ double(&BB)[m][n], int s2/*, int c*/) {
 	// if c == 1 znachit berem stroku if c== 2 znachit berem stolbec
 	// s eto nomer stroki ili stolbca kotoriy nuzhno vzat
-   // if(c==1){
-	for (int j = 0; j < m; j++) {
-		k[j] = AA[s1][j];
+
+	//if(c==1){
+	//for (int j = 0; j < m; j++) {
+	//	k[j] = AA[s1][j];
 		//cout << k[j] << endl;
-	}
-	//  }else if(c==2){
+	//}
+	 // }else if(c==2){
 	for (int j = 0; j < m; j++) {
 		l[j] = BB[j][s2];
 		//cout << l[j] << endl;
 	}
 	//}
+
 }
 
 double peremnoj_vector_na_vector(double(&kk)[m], double(&ll)[m]) {
@@ -168,9 +172,16 @@ double peremnoj_vector_na_vector(double(&kk)[m], double(&ll)[m]) {
 	return h;
 }
 
-double Temp[] = { 0,0,0 };
+double Temp[n];
 int Mesto[] = { 0,0 };
 int N = 0, M = 0, Nn = 0, Mm = 0, limit_1 = 0;
+//int kol_strok_v_posled_str_bloke = 0;
+//int kol_stolb_v_posled_stolb_bloke = 0;
+//bool is_n_menwe_size;
+
+
+
+
 
 // make matrix multiplying with MPI_Reduce or MPI_AllReduce 
 int main() {
@@ -186,6 +197,68 @@ int main() {
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+
+	if (rank == 0) {
+		FillMatrix(A, B);
+		//FillVector(v);
+		Zapis_v_File();
+		//read_Vector();
+		read_Matrix();
+	}
+
+	double  rbufA[m], rbufB[m]; int gsize;
+	MPI_Comm_size(MPI_COMM_WORLD, &gsize);
+
+	MPI_Bcast(B1, n*m, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatter(A1, m, MPI_DOUBLE, rbufA, m, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	
+	double buff[1000];
+	MPI_Buffer_attach(&buff, n * m * sizeof(double));
+	double buff2[1000];
+	MPI_Buffer_attach(&buff2, n * m * sizeof(double));
+
+
+
+	for (int i = 0; i < 4; i++) {
+		//double kk[m],kkk[m];
+		vzat_vector_iz_matrix(B1, i);
+		//cout <<rbufB[i]<<endl;
+		Temp[i] = peremnoj_vector_na_vector(rbufA, l);
+
+		cout << Temp[i] << " ";
+
+	}
+	cout << endl;
+	double rbuf[n * n];
+	MPI_Gather(&Temp[0], 4, MPI_DOUBLE, rbuf, 4, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	if (rank == 0) {
+
+		//cout <<"joparabotai"<< 228<<endl;
+		int g = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+
+				cout << rbuf[g] << endl;
+
+				C[i][j] = rbuf[g];
+
+				fflush(stdout);
+				g++;
+			}
+
+		}
+		Zapix_otvetov_v_File(C/*,d*/);
+
+	}
+
+
+
+
+
+
+
+
+	
 
 	endtime = MPI_Wtime();
 	printf("vipolnenie zanyalo %f seconds\n", endtime - starttime);
